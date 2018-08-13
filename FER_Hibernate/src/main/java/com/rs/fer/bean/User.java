c06ae9aa466cd18ed3670c47add1e9e30d5db1f5
AMLAN <AMLAN@DESKTOP-C2CFJED>
2018-08-13 18:35:53 +0530

    FER_hibernate_bean

---
+++ FER_Hibernate/src/main/java/com/rs/fer/bean/User.java
@@ -0,0 +1,101 @@
+package com.rs.fer.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "firstName")
+	private String firstName;
+	@Column(name = "middleName")
+	private String middleName;
+	@Column(name = "lastName")
+	private String lastName;
+	@Column(name = "emailId")
+	private String emailId;
+
+	@Column(name = "userName")
+	private String userName;
+	
+	@Column(name = "userWork")
+	private String userWork;
+
+	@Column(name = "passWord")
+	private String passWord;
+
+	@Column(name = "mobileNumber")
+	private String mobileNumber;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+}
