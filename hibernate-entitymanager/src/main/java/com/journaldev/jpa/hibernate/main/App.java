0e2b36296d6b032b1e0307a7cff1d809f1a8b2b1
AMLAN <w.amlandas@gmail.com>
2018-08-13 16:40:10 +0530

    entitymanager

---
+++ hibernate-entitymanager/src/main/java/com/journaldev/jpa/hibernate/main/App.java
@@ -0,0 +1,52 @@
+package com.journaldev.jpa.hibernate.main;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.journaldev.jpa.hibernate.model.Employee;
+
+public class App {
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		System.out.println("Starting Transaction");
+		entityManager.getTransaction().begin();
+		Employee employee = new Employee();
+		employee.setName("Amlan");
+		System.out.println("Saving Employee to Database");
+
+		entityManager.persist(employee);
+		entityManager.getTransaction().commit();
+		System.out.println("Generated Employee ID = " + employee.getEmployeeId());
+
+		// get an object using primary key.
+		Employee emp = entityManager.find(Employee.class, employee.getEmployeeId());
+		System.out.println("got object " + emp.getName() + " " + emp.getEmployeeId());
+
+		// get all the objects from Employee table
+		@SuppressWarnings("unchecked")
+		List<Employee> listEmployee = entityManager.createQuery("SELECT e FROM Employee e").getResultList();
+
+		if (listEmployee == null) {
+			System.out.println("No employee found . ");
+		} else {
+			for (Employee empl : listEmployee) {
+				System.out.println("Employee name= " + empl.getName() + ", Employee id " + empl.getEmployeeId());
+			}
+		}
+		// remove and entity
+		/*entityManager.getTransaction().begin();
+		System.out.println("Deleting Employee with ID = " + emp.getEmployeeId());
+		entityManager.remove(emp);
+		entityManager.getTransaction().commit();*/
+
+		// close the entity manager
+		entityManager.close();
+		entityManagerFactory.close();
+
+	}
+}
---
+++ hibernate-entitymanager/src/main/java/com/journaldev/jpa/hibernate/model/Employee.java
@@ -0,0 +1,42 @@
+package com.journaldev.jpa.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+	private int employeeId;
+
+	private String name;
+
+	@Id
+	@Column(name = "employee_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	@Column(name = "employee_name")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", name=" + name + "]";
+	}
+
+}
